68ea21ede15d4c1b3ae1c600e0cc39e86945ec1d
Dzenan Cindrak <dzenancindrak@gmail.com>
2021-04-15 18:30:10 +0200

    IP address owner history with APSScheduler

--- backend/proxmox_api/__main__.py
+++ backend/proxmox_api/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import connexion
 from flask_cors import CORS
+from flask_apscheduler import APScheduler
 
 import proxmox_api.config.configuration as config
 from proxmox_api import encoder
@@ -9,20 +10,42 @@ app = connexion.App(__name__, specification_dir='./swagger/')
 
 app.app.json_encoder = encoder.JSONEncoder
 app.app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
+
 CORS(app.app)
+
+scheduler = APScheduler()
+
 app.add_api('swagger.yaml', arguments={'title': 'Proxmox'}, pythonic_params=True)
 
+JOBS = [
+        {
+            "id": "update_vm_ips",
+            "func": "proxmox_api.proxmox:update_vm_ips_job",
+            "args": (app.app,),
+            "trigger": "interval",
+            "seconds": 5,
+        }
+    ]
+
+app.app.config['JOBS'] = JOBS
+app.app.config['SCHEDULER_API_ENABLE'] = False
+
+
 from proxmox_api.db.db_models import db
+
 
 
 ## init db
 
 db.init_app(app.app)
 with app.app.app_context():
-    #db.drop_all()
+    db.drop_all()
     db.create_all()
 
+scheduler.init_app(app.app)
 
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    scheduler.start()
+    app.run(port=8080, debug=False)
+
