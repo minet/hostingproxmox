b2e135abdf986f528464305187b22dd8a92a0a26
Dzenan CINDRAK <dzenan.cindrak@telecom-sudparis.eu>
2021-05-04 20:52:20 +0200

    Scroll to ssh view when clicking button + routerRedirect when clicking on buttons admin home

--- frontend/src/app/vms/vms.component.ts
+++ frontend/src/app/vms/vms.component.ts
@@ -7,87 +7,98 @@ import {User} from '../models/user';
 import {SlugifyPipe} from '../pipes/slugify.pipe';
 import {interval, Observable, Subscription, timer} from 'rxjs';
 import {flatMap} from 'rxjs/internal/operators';
+import {ViewportScroller} from '@angular/common';
+
 @Component({
-  selector: 'app-vms',
-  templateUrl: './vms.component.html',
-  styleUrls: ['./vms.component.css']
+    selector: 'app-vms',
+    templateUrl: './vms.component.html',
+    styleUrls: ['./vms.component.css']
 })
 export class VmsComponent implements OnInit, OnDestroy {
-  loading = true;
-  intervals = new Set<Subscription>();
-  showSsh = false;
-  page = 1;
-  pageSize = 10;
-  constructor(private http: HttpClient,
-              public user: User,
-              private userService: UserService,
-              public authService: AuthService,
-              public slugifyPipe: SlugifyPipe) {
-  }
+    loading = true;
+    intervals = new Set<Subscription>();
+    showSsh = false;
+    page = 1;
+    pageSize = 5;
 
-ngOnInit(): void {
-    this.userService.getUser().subscribe((user) => this.user = user);
-    this.user.vms = Array<Vm>();
-    this.get_vms();
-  }
-
-  ngOnDestroy(): void {
-    for (const id of this.intervals) {
-      id.unsubscribe();
+    constructor(private http: HttpClient,
+                public user: User,
+                private userService: UserService,
+                public authService: AuthService,
+                public slugifyPipe: SlugifyPipe,
+                private vps: ViewportScroller) {
     }
-  }
 
-  get_vms(): void {
-    let vmList: Array<string>;
-    this.http.get(this.authService.SERVER_URL + '/vm', {observe: 'response'}).subscribe(rep => {
-        vmList = rep.body as Array<string>;
-        console.log(vmList);
-        if (vmList.length === 0) {
-          this.loading = false;
+    ngOnInit(): void {
+        this.userService.getUser().subscribe((user) => this.user = user);
+        this.user.vms = Array<Vm>();
+        this.get_vms();
+    }
+
+    ngOnDestroy(): void {
+        for (const id of this.intervals) {
+            id.unsubscribe();
         }
-        for (let i = 0; i < vmList.length; i++) {
-          const vmid = vmList[i];
-          const last = (i === vmList.length - 1);
-          this.get_vm(vmid, last);
-          console.log(vmid);
+    }
+
+    scrollSsh($element): void {
+        this.showSsh = !this.showSsh;
+        if (this.showSsh) {
+            $element.scrollIntoView({behavior: 'auto', block: 'start', inline: 'start'});
         }
-      },
-      error => {
+    }
 
-      });
+    get_vms(): void {
+        let vmList: Array<string>;
+        this.http.get(this.authService.SERVER_URL + '/vm', {observe: 'response'}).subscribe(rep => {
+                vmList = rep.body as Array<string>;
+                console.log(vmList);
+                if (vmList.length === 0) {
+                    this.loading = false;
+                }
+                for (let i = 0; i < vmList.length; i++) {
+                    const vmid = vmList[i];
+                    const last = (i === vmList.length - 1);
+                    this.get_vm(vmid, last);
+                    console.log(vmid);
+                }
+            },
+            error => {
 
-  }
+            });
 
-  get_vm(vmid: string, last: boolean): void {
-    const vm = new Vm();
-    vm.id = vmid;
-    this.user.vms.push(vm);
-    const newTimer = timer(0, 3000).pipe(
-      flatMap(() => this.http.get(this.authService.SERVER_URL + '/vm/' + vmid, {observe: 'response'})))
-      .subscribe(rep => {
-          vm.name = rep.body['name'];
-          vm.status = rep.body['status'];
-          vm.user = rep.body['user'];
-          if (rep.body['type'] === 'nginx_vm') {
-            vm.type = 'web server';
-          } else if (rep.body['type'] === 'bare_vm') {
-            vm.type = 'bare vm';
-          } else {
-            vm.type = 'not defined';
-          }
+    }
 
-          if (vm.status === 'running') {
-            vm.ip = rep.body['ip'][0];
-          }
+    get_vm(vmid: string, last: boolean): void {
+        const vm = new Vm();
+        vm.id = vmid;
+        this.user.vms.push(vm);
+        const newTimer = timer(0, 3000).pipe(
+            flatMap(() => this.http.get(this.authService.SERVER_URL + '/vm/' + vmid, {observe: 'response'})))
+            .subscribe(rep => {
+                    vm.name = rep.body['name'];
+                    vm.status = rep.body['status'];
+                    vm.user = rep.body['user'];
+                    if (rep.body['type'] === 'nginx_vm') {
+                        vm.type = 'web server';
+                    } else if (rep.body['type'] === 'bare_vm') {
+                        vm.type = 'bare vm';
+                    } else {
+                        vm.type = 'not defined';
+                    }
 
-          if (last) {
-            this.loading = false;
-          }
-        },
-        error => {
+                    if (vm.status === 'running') {
+                        vm.ip = rep.body['ip'][0];
+                    }
 
-        });
-    this.intervals.add(newTimer);
-  }
+                    if (last) {
+                        this.loading = false;
+                    }
+                },
+                error => {
+
+                });
+        this.intervals.add(newTimer);
+    }
 
 }
