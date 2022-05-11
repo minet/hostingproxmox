0f8825e553521077646dc006b527c0c304575051
seaweedbrain <seaweedbrain@minet.net>
2022-05-11 16:34:47 +0200

    Minor fix in sshd config patch

--- backend/patch/ssh_patch.sh
+++ backend/patch/ssh_patch.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo [*] Downloading new sshd config ...
-wget https://raw.githubusercontent.com/minet/hostingproxmox/master/backend/sshd_config
+wget https://raw.githubusercontent.com/minet/hostingproxmox/master/backend/patch/sshd_config
 echo [*] Installing sshd_config
 mv -f sshd_config /etc/ssh/
 echo [*] Restarting sshd server ...
