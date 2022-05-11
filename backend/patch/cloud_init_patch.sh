49a7422d5971248ea660b04960091aef82163037
seaweedbrain <seaweedbrain@minet.net>
2022-05-11 16:14:49 +0200

    Minor fix in cloud init patch

--- backend/patch/cloud_init_patch.sh
+++ backend/patch/cloud_init_patch.sh
@@ -1,11 +1,11 @@
-#8 /bin/bash
+#! /bin/bash
 
 echo [*] List users ...
 ls /home/ > /tmp/users.txt
 touch /tmp/rules.txt
 echo \# Created by cloud-init v. 20.2 on $(date) >> /tmp/rules.txt
 echo "" >> /tmp/rules.txt
-while read user; do 
+while read user; do
 	echo '# Rules for user' $user >> /tmp/rules.txt
 	echo $user 'ALL=(ALL) ALL' >>  /tmp/rules.txt
 done < /tmp/users.txt
@@ -24,8 +24,8 @@ rm /tmp/rules.txt
 rm /tmp/users.txt
 
 if grep -q NOPASSWD /etc/sudoers.d/90-cloud-init-users; then
-	echo [*] An error occured. Please contact webmaster@minet.net 
-else 
+	echo [*] An error occured. Please contact webmaster@minet.net
+else
 	echo [*] Patch applied with success
 fi
 echo [*] END
