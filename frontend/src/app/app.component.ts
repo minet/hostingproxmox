8c1713990b5417c0a57a90ac8459afdb74940e87
seaweedbrain <seaweedbrain@minet.net>
2022-06-19 22:22:34 +0200

    Fix the favicon Add different page title for hosting, hostind dev and hosting local

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { UserService } from './common/services/user.service';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,25 @@ export class AppComponent implements OnInit{
   title = 'VPS MiNET';
 
 
-  constructor(public userService: UserService, public router: Router) {
+  constructor(public userService: UserService, public router: Router, private titleService: Title) {
+    var url = window.location.hostname;
+    var title = "Hosting";
+    
+    switch(url){
+      case "hotsing-dev.minet.net" : {
+        title = "Hosting Dev";
+        break;
+      }
+      case "hosting-local.minet.net" :{
+         title = "Hosting local";
+         break;
+      }
+      default: {
+        title = "Hosting";
+        break;
+      } 
+    }
+    this.titleService.setTitle(title)
   }
 
   public validToken$: Observable<boolean>;
