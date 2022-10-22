9fb38913e054d91c070ae5eeefc7b16cd2c4d530
seaweedbrain <seaweedbrain@minet.net>
2022-10-22 23:37:20 +0200

    Add the french translation for the handling of freeze status

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit {
   prefix: string;
   loading = false;
   valide = false;
-  errorcode = 0;
+  errorcode = -1;
   errorMessage = ""
   url: string;
   sshAddress: string;
