5f0a66000a0affadf7a735bb23d3af83ca6f7c8d
seaweedbrain <seaweedbrain@minet.net>
2023-01-04 00:45:45 +0100

    make vm_creation_status.json be created if it doesn't exist

--- backend/proxmox_api/util.py
+++ backend/proxmox_api/util.py
@@ -214,7 +214,7 @@ def check_dns_entry(entry:str) -> bool:
     :rtype:  Tuple[str, str, str]
 """
 def get_vm_state(vmid) :
-    with open(config.VM_CREATION_STATUS_JSON, mode='r+') as jsonFile:
+    with open(config.VM_CREATION_STATUS_JSON, mode='a+') as jsonFile:
         jsonObject = json.load(jsonFile)
         jsonFile.close()
     try :
@@ -251,7 +251,7 @@ def get_vm_state(vmid) :
     :rtype: bool
 """
 def update_vm_state(vmid, message, errorCode = 0, deleteEntry = False) -> bool:
-    with open(config.VM_CREATION_STATUS_JSON, mode='r+') as jsonFile:
+    with open(config.VM_CREATION_STATUS_JSON, mode='a+') as jsonFile:
         jsonObject = json.load(jsonFile)
         jsonFile.close()
 
