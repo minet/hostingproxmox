226571a81640abc44f240a432d665c4a3682faa4
seaweedbrain <seaweedbrain@minet.net>
2023-02-01 13:23:27 +0100

    rebase resolve 30/33

--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -13,7 +13,13 @@ export class User {
     public vms?: Array<Vm>;
     public dns?: Array<Dns>;
     public ips?: Array<string>;
+<<<<<<< HEAD
     public usedCPU?: number;
     public usedRAM?: number;
     public usedStorage?: number;
+=======
+    public availableCPU?: number;
+    public availableRAM?: number;
+    public availableStorage?: number;
+>>>>>>> 6ce4559 (Add prototype of dynamix ressources form in hom component)
 }
