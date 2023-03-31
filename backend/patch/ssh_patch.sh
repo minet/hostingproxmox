1058e3c73f4de0da6fa7c9c8a7bf1f56b306437c
seaweedbrain <seaweedbrain@minet.net>
2023-04-01 01:58:55 +0200

    Disable ssh key verification in ssh patch

--- backend/patch/ssh_patch.sh
+++ backend/patch/ssh_patch.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-if ! [ -s ~/.ssh/authorized_keys ]; then
-	echo [!] WARNING : You have to import your ssh keys in this VM BEFORE any security patch. SSH connection with ssh keys will be mandatory.
-       exit 1
-fi
-
 
 echo [*] Downloading new sshd config ...
 wget https://raw.githubusercontent.com/minet/hostingproxmox/master/backend/patch/sshd_config
