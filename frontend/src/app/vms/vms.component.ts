14bc9d76ad84f6d6b5af1e9a704f57ee3d191fed
Valt7 <v.lantigny@gmail.com>
2024-03-31 23:18:49 +0200

    AIDEZ

--- frontend/src/app/vms/vms.component.ts
+++ frontend/src/app/vms/vms.component.ts
@@ -146,7 +146,7 @@ export class VmsComponent implements OnInit, OnDestroy {
                     vm.user = rep.body['user'];
                     vm.ip = rep.body['ip'][0];
                     vm.uptime = rep.body['uptime'];
-                    vm.lastBackupDate = rep.body['last_backup_date']
+                    // vm.lastBackupDate = rep.body['last_backup_date']
 
                     vm.createdOn = rep.body['created_on'];
                     if (rep.body['type'] === 'nginx_vm') {
