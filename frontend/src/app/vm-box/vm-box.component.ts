7b895ff01286b1ab12db0405f0187363b902260c
Valt7 <v.lantigny@gmail.com>
2024-04-19 23:46:07 +0200

    fix wrong visual

--- frontend/src/app/vm-box/vm-box.component.ts
+++ frontend/src/app/vm-box/vm-box.component.ts
@@ -12,6 +12,24 @@ export class VmBoxComponent {
   @Input() user!: User;
 
 
-  
+  /**
+   * Convert seconds to days, hours, minutes and seconds
+   * @param seconds
+   * @returns string
+   * @example 90061 => 1d 1h 1min 1s
+  */
+  secondsToDhms(seconds: number): string {
+    seconds = Number(seconds);
+    const d = Math.floor(seconds / (3600 * 24));
+    const h = Math.floor(seconds % (3600 * 24) / 3600);
+    const m = Math.floor(seconds % 3600 / 60);
+    const s = Math.floor(seconds % 60);
+
+    const dDisplay = d > 0 ? d + 'd ' : '';
+    const hDisplay = h > 0 ? h + 'h ' : '';
+    const mDisplay = m > 0 ? m + 'min ' : '';
+    const sDisplay = s > 0 ? s + 's ' : '';
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+}
 
 }
