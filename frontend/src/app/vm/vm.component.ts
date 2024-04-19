c6c9d0bb9d6747f9559d5668e7c6e4cb78d56f57
Valt7 <v.lantigny@gmail.com>
2024-04-19 23:06:03 +0200

    fix wrong visual

--- frontend/src/app/vm/vm.component.ts
+++ frontend/src/app/vm/vm.component.ts
@@ -134,8 +134,27 @@ export class VmComponent implements OnInit, OnDestroy {
         const minutes = date.getMinutes().toString().padStart(2, '0');
         
         return `${year}-${month}-${day} ${hours}:${minutes}`;
-      }
+    }
 
+    /**
+       * Convert seconds to days, hours, minutes and seconds
+       * @param seconds
+       * @returns string
+       * @example 90061 => 1d 1h 1min 1s
+      */
+      secondsToDhms(seconds: number): string {
+        seconds = Number(seconds);
+        const d = Math.floor(seconds / (3600 * 24));
+        const h = Math.floor(seconds % (3600 * 24) / 3600);
+        const m = Math.floor(seconds % 3600 / 60);
+        const s = Math.floor(seconds % 60);
+    
+        const dDisplay = d > 0 ? d + 'd ' : '';
+        const hDisplay = h > 0 ? h + 'h ' : '';
+        const mDisplay = m > 0 ? m + 'min ' : '';
+        const sDisplay = s > 0 ? s + 's ' : '';
+        return dDisplay + hDisplay + mDisplay + sDisplay;
+    }
 
     commit_edit(status: string): void {
         const data = {
